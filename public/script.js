5edfe1aae12de731ea10252ee85506f242c61d2c
Arisa <uliannaobr@gmail.com>
2023-01-24 22:42:24 +0400

    create server

---
+++ public/script.js
@@ -0,0 +1,5 @@
+fetch("/events")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+    })
---
+++ server.js
@@ -0,0 +1,33 @@
+const PORT = process.env.PORT || 2000;
+const XMLHttpRequest = require('xhr2');
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.Server(app);
+const path = require('path');
+const bp = require('body-parser');
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+server.listen(PORT, function () {
+    console.log('Server port 2000');
+});
+
+app.get('/events', (req, res) => {
+    console.log(req.url);
+    let request = new XMLHttpRequest();
+    let url = "https://opendata.mkrf.ru/v2/events/$?l=20";
+    request.open("GET", url, true);
+    request.setRequestHeader("X-API-KEY", "d5aa62ce093df5123895e5032d2f922a759e63cacf673ecd601b34c97a0c316e");
+    request.send(null);
+    request.onreadystatechange = () => {
+        if (request.readyState == 4) {
+            console.log(request);
+            res.send(request.response);
+        }
+    };
+})
