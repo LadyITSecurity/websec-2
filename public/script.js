d22f4535533fc586ed5ca8786e080d510e698399
Arisa <uliannaobr@gmail.com>
2023-01-25 11:54:16 +0400

    ended create server, create client script file, minimum data show

--- public/script.js
+++ public/script.js
@@ -2,4 +2,43 @@ fetch("/events")
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
+        generateHTML(data.data);
     })
+
+function generateHTML(data) {
+    let allEvents = document.querySelector(".events-container");
+
+    for (let event of data) {
+        let eventElem = document.createElement('div');
+        eventElem.classList.add('event');
+
+        let title = document.createElement('p');
+        title.classList.add('event-title');
+        title.innerHTML = event.data.general.name;
+
+        let imageElem = document.createElement('img');
+        imageElem.setAttribute("src", event.data.general.image.url);
+        imageElem.style.maxHeight = "100%";
+        imageElem.style.maxWidth = "100%";
+
+        let shortDescription = document.createElement('p');
+        shortDescription.classList.add('event-desc');
+        shortDescription.innerHTML = event.data.general.shortDescription;
+
+
+        let fullAddress = document.createElement('p');
+        fullAddress.classList.add('event-address');
+        fullAddress.innerHTML = event.data.general.fullAddress;
+        
+
+        eventElem.appendChild(title);
+        eventElem.appendChild(imageElem);
+        eventElem.appendChild(shortDescription);
+
+        allEvents.appendChild(eventElem);
+
+        console.log(title);
+        console.log(imageElem);
+        console.log(shortDescription);
+    }
+}
